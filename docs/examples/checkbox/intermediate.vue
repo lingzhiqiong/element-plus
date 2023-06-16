b276397e5970d0fd234dbda35bc13ac87c800f84
lingzhiqiong <117741395+lingzhiqiong@users.noreply.github.com>
2023-06-16 10:52:41 +0800

    Update intermediate.vue

--- docs/examples/checkbox/intermediate.vue
+++ docs/examples/checkbox/intermediate.vue
@@ -21,7 +21,27 @@ import { ref } from 'vue'
 const checkAll = ref(false)
 const isIndeterminate = ref(true)
 const checkedCities = ref(['Shanghai', 'Beijing'])
-const cities = ['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen']
+const cities = [ {
+          props: "name",
+          columnName: "候选人姓名",
+          columnText: "候选人的姓名",
+          isShow: true,
+          width: "150px"
+        },
+        {
+          props: "mobile",
+          columnName: "手机号码",
+          columnText: "候选人的手机号码",
+          isShow: true,
+          width: "150px"
+        },
+        {
+          props: "organizationUnit",
+          columnName: "应聘部门",
+          columnText: "候选人的应聘的部门名称",
+          isShow: true,
+          width: "150px"
+        },]
 
 const handleCheckAllChange = (val: boolean) => {
   checkedCities.value = val ? cities : []
